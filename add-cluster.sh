9b73f0963fac991efd641062f81c6e461ff3668d
Johan Hernefeldt <3883897+presidenten@users.noreply.github.com>
2021-12-22 10:56:15 +0100

    Added nginx ingress controller

--- add-cluster.sh
+++ add-cluster.sh
@@ -55,6 +55,7 @@ eval "${AGENTCOMMAND}"
 
 ### Extra stuff
 kubectl wait --for=condition=Ready --timeout=120s pods --all --all-namespaces
+sh scripts/setup-ingress-controller.sh
 sh scripts/setup-monitoring.sh
 sh scripts/setup-linkerd.sh
 
